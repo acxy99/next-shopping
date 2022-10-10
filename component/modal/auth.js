881b7cab3a3f3a15781b5334c478046736d9a14c
aaccxxyy9999 <alicechin9017@gmail.com>
2022-10-10 15:43:55 +0800

    add sign/register

--- component/modal/auth.js
+++ component/modal/auth.js
@@ -1,61 +1,74 @@
 import Modal from "react-bootstrap/Modal";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Tab, Nav } from "react-bootstrap";
 import { XLg } from "react-bootstrap-icons";
+import SignIn from "../auth-tabs/SignIn";
+import Register from "../auth-tabs/Register";
 const ModalAuth = () => {
   return (
     <Modal
       show={true}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
       className="modal-auth"
     >
       <Modal.Body>
         <div className="p-4">
-          <Row>
-            <Col md={12} className="text-end">
-              <XLg style={{ fontSize: "20px" }} />
-            </Col>
-            <Col md={12}>
-              <h1
-                className="m-0 font-weight-bold d-inline-flex "
-                style={{ fontWeight: "bolder", fontSize: "20px" }}
-              >
-                <span style={{ fontWeight: "normal" }}>Welcome to&nbsp;</span>
-                <span
-                  className="border px-2 mr-1"
-                  style={{ color: "#98c1d9", fontWeight: "bolder" }}
-                >
-                  N
-                </span>
-                <span>extShop</span>
-              </h1>
-            </Col>
-            <Col md={12} className="pt-3">
-              {/* Email */}
-              <div class="form-floating mb-3">
-                <input
-                  type="email"
-                  className="form-control input-auth"
-                  id="floatingInput"
-                  placeholder="Enter your Email Adress"
-                />
-                <label for="floatingInput">Email Address</label>
-              </div>
-            </Col>
-            <Col md={12} className="pt-2">
-              {/* password */}
-              <div class="form-floating">
-                <input
-                  type="password"
-                  className="form-control input-auth"
-                  id="floatingPassword"
-                  placeholder="Password"
-                />
-                <label for="floatingPassword">Password</label>
-              </div>
-            </Col>
-            <Col md={12}>{/* Submit Buttion */}</Col>
+          <Row className="justify-content-around">
+            <Tab.Container
+              id="left-tabs-example"
+              activeKey="signIn"
+              defaultActiveKey="signIn"
+            >
+              <Row>
+                <Col md={12} className="d-flex  align-items-center pb-3">
+                  <Col md={11} className="">
+                    <Nav
+                      variant="pills"
+                      className="flex-row justify-content-center "
+                    >
+                      <Nav.Item className="auth-tabs me-1">
+                        <Nav.Link eventKey="signIn">Sign In</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item className="auth-tabs">
+                        <Nav.Link eventKey="register">Register</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                  </Col>
+                  <Col md={1} className="text-end">
+                    {" "}
+                    <XLg style={{ fontSize: "20px" }} />
+                  </Col>
+                </Col>
+                <Col md={12}>
+                  <h1
+                    className="m-0 font-weight-bold d-inline-flex "
+                    style={{ fontWeight: "bolder", fontSize: "20px" }}
+                  >
+                    <span style={{ fontWeight: "normal" }}>
+                      Welcome to&nbsp;
+                    </span>
+                    <span
+                      className="border px-2 mr-1"
+                      style={{ color: "#98c1d9", fontWeight: "bolder" }}
+                    >
+                      N
+                    </span>
+                    <span>extShop</span>
+                  </h1>
+                </Col>
+                <Col md={12}>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="signIn">
+                      <SignIn />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="register">
+                      <Register />
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
           </Row>
         </div>
       </Modal.Body>
--- component/modal/register.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ModalRegister() {
-  return (
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"></div>
-  );
-}
-
-export default ModalRegister;
